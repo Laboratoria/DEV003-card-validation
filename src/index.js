a174c7e76c020ae388a736d1fd1f467d66a7218e
Andry-Salazar <113132734+Andry-Salazar@users.noreply.github.com>
2022-12-07 14:23:01 -0400

    proyecto card validation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+console.log(validator.maskify("4556364607935616"));
+console.log(validator.maskify("64607935616"));
+console.log(validator.maskify("1"));
+console.log(validator.maskify(""));
+
+console.log(validator.isValid("64607935616"));
+console.log(validator.isValid("4083952015263"));
+console.log(validator.isValid("79927398713"));
+
+const password = document.getElementById("cardNumber");
+const cvvEl = document.getElementById("cvv");
+
+// validator.setInputFilter(
+//   password,
+//   (value) => {
+//     return /^\d*$/.test(value);
+//   },
+//   "Solo dígitos"
+// );
+
+validator.setInputFilter(
+  cvvEl,
+  (value) => {
+    // regex
+    return /^\d*$/.test(value);
+  },
+  "Solo dígitos"
+);
+
+password.addEventListener("input", (event) => {
+  const el = event.target;
+  // console.log(el, el.value, validator.isValid(el.value));
+  if (!validator.isValid(el.value) && el.value.length >= 12) {
+    el.setCustomValidity("La tarjeta no es válida");
+    el.reportValidity();
+  } else {
+    el.setCustomValidity("");
+  }
+});
+
+password.addEventListener("blur", (event) => {
+  const el = event.target;
+  el.maskedValue = el.value;
+  el.value = validator.maskify(el.value);
+  el.removeOnlyDigitsEvents();
+});
+
+password.addEventListener("focus", (event) => {
+  const el = event.target;
+  el.value = el.maskedValue || "";
+  el.removeOnlyDigitsEvents = validator.setInputFilter(
+    password,
+    (value) => /^\d*$/.test(value),
+    "Solo dígitos"
+  );
+});
+
+// validator.setInputFilter(
+//   valueNumberCard,
+//   (value) => {
+//     return validator.isValid(value);
+//   },
+//   "La tarjeta no es válida"
+// );
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,74 @@
 const validator = {
-  // ...
+  isValid: function (array) {
+    let numPar = 0;
+    let sumArr = 0;
+    for (let i = 0; i < array.length; i++) {
+      let sumDigito = 0;
+      if (i % 2 === 1) {
+        let numeroDoble = Number(array[i]) * 2;
+        numPar = numeroDoble % 10;
+        numeroDoble = parseInt(numeroDoble / 10);
+        sumDigito = numPar + numeroDoble;
+      } else {
+        sumDigito = Number(array[i]);
+      }
+      sumArr += sumDigito;
+    }
+
+    return sumArr % 10 === 0;
+  },
+
+  maskify: function (creditCardNumber) {
+    const length = creditCardNumber.length;
+    const cardMaskify = creditCardNumber.substring(length - 4, length);
+    return cardMaskify.padStart(length, "#");
+  },
+
+  setInputFilter: function (inputEl, isValid, errMsg) {
+    const listener = function (e) {
+      if (isValid(this.value)) {
+        // Accepted value.
+        if (["keydown", "mousedown", "focusout"].indexOf(e.type) >= 0) {
+          this.classList.remove("input-error");
+          this.setCustomValidity("");
+        }
+
+        this.oldValue = this.value;
+        // } else if (this.hasOwnProperty("oldValue")) {
+      } else if (this.oldValue) {
+        // Rejected value: restore the previous one.
+        this.classList.add("input-error");
+        this.setCustomValidity(errMsg);
+        this.reportValidity();
+        this.value = this.oldValue;
+      } else {
+        // Rejected value: nothing to restore.
+        this.value = "";
+      }
+    };
+
+    const events = [
+      "input",
+      "keydown",
+      "keyup",
+      "mousedown",
+      "mouseup",
+      "select",
+      "contextmenu",
+      "drop",
+      "focusout",
+    ];
+
+    events.forEach(function (event) {
+      inputEl.addEventListener(event, listener);
+    });
+
+    return () => {
+      events.forEach((evtName) => {
+        inputEl.removeEventListener(evtName, listener);
+      });
+    };
+  },
 };
 
 export default validator;
